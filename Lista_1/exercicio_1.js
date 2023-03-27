30936c71204eebe5dc8c4c53a0dadb1f573ce451
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-03-26 23:44:47 -0300

    Lista 1 pronta

---
+++ Lista_1/exercicio_1.js
@@ -0,0 +1,17 @@
+var nome1 = "Miguel"
+var idade1 = 22
+var nome2 = "Carlos"
+var idade2 = 13
+var nome3 = "Ana"
+var idade3 = 49
+var nome4 = "José"
+var idade4 = 37
+var nome5 = "Marcia"
+var idade5 = 71
+var anos = " anos"
+
+console.log(nome1 + " - " + idade1 + anos)
+console.log(nome2 + " - " + idade2 + anos)
+console.log(nome3 + " - " + idade3 + anos)
+console.log(nome4 + " - " + idade4 + anos)
+console.log(nome5 + " - " + idade5 + anos)
---
+++ Lista_1/exercicio_1.md
@@ -0,0 +1,6 @@
+#  Lista de 5 extensões que usamos em aula:
+    - .html
+    - .js
+    - .cs
+    - .java
+    - .por
