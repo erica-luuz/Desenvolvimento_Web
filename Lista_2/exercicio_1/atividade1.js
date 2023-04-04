505cc675dad31a80c0862e6d1d812f132cd4fe66
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-04-04 20:55:08 -0300

    Conclusão do exercicio 1

---
+++ Lista_2/exercicio_1/atividade1.js
@@ -0,0 +1,28 @@
+var Reajuste = 10
+var salarioTrabalhador1 = 1000
+var valorReajustetrabalhador1 = salarioTrabalhador1 + (salarioTrabalhador1 * Reajuste / 100)
+
+var salarioTrabalhador2 = 2000
+var valorReajustetrabalhador2 = salarioTrabalhador2 + (salarioTrabalhador2 * Reajuste / 100)
+
+var salarioTrabalhador3 = 3000
+var valorReajustetrabalhador3 = salarioTrabalhador3 + (salarioTrabalhador3 * Reajuste / 100)
+
+var salarioTrabalhador4 = 4000
+var valorReajustetrabalhador4 = salarioTrabalhador4 + (salarioTrabalhador4 * Reajuste / 100)
+
+var salarioTrabalhador5 = 5000
+var valorReajustetrabalhador5 = salarioTrabalhador5 + (salarioTrabalhador5 * Reajuste / 100)
+
+var nomeTrabalhador1 = "Larissa"
+var nomeTrabalhador2 = "Maria"
+var nomeTrabalhador3 = "Jonas"
+var nomeTrabalhador4 = "Francisco"
+var nomeTrabalhador5 = "João"
+
+
+console.log(nomeTrabalhador1, "ganha R$", salarioTrabalhador1, ", com reajuste de 10% passa a ganhar R$", valorReajustetrabalhador1)
+console.log(nomeTrabalhador2, "ganha R$", salarioTrabalhador2, ", com reajuste de 10% passa a ganhar R$", valorReajustetrabalhador2)
+console.log(nomeTrabalhador3, "ganha R$", salarioTrabalhador3, ", com reajuste de 10% passa a ganhar R$", valorReajustetrabalhador3)
+console.log(nomeTrabalhador4, "ganha R$", salarioTrabalhador4, ", com reajuste de 10% passa a ganhar R$", valorReajustetrabalhador4)
+console.log(nomeTrabalhador5, "ganha R$", salarioTrabalhador5, ", com reajuste de 10% passa a ganhar R$", valorReajustetrabalhador5)
