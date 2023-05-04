7ea43f8baa619d22cded4f1a5fc47b9937881230
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-05-03 23:19:32 -0300

    Inicio do exercicio01 funções JS

---
+++ Listas/Lista_6/exercicio01/js/exercicio01.js
@@ -0,0 +1,28 @@
+let listaPessoas = [
+    {
+        nome: "Fulano",
+        sobrenome: "Santos",
+        idade: 18
+    },
+    {
+        nome: "Ciclano",
+        sobrenome: "Ribeiro",
+        idade: 17
+
+    },
+    {
+        nome: "Beltrano",
+        sobrenome: "Silva",
+        idade: 16
+    },
+    {
+        nome: "Herculano",
+        sobrenome: "Menezes",
+        idade: 20
+    },
+    {
+        nome: "Minuano",
+        sobrenome: "Souza",
+        idade: 13
+    }
+]
