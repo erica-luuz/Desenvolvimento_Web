2b0257812b9ed24a1636e48c2b2fe78e8fca9e41
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-05-05 20:59:28 -0300

    Exercicio concluido com sucesso!

--- Listas/Lista_6/exercicio01/js/exercicio01.js
+++ Listas/Lista_6/exercicio01/js/exercicio01.js
@@ -1,3 +1,32 @@
+function maiorIdade(listaPessoas) {
+    for (let i = 0; i < listaPessoas.length; i++) {
+        if (listaPessoas[i].idade >= 18) {
+            console.log(`Seja bem vindo(a)! Você se chama ${listaPessoas[i].nome} ${listaPessoas[i].sobrenome} e tem ${listaPessoas[i].idade} anos e é maior de idade.`)
+        } else {
+            console.log(`Seja bem vindo(a)! Você se chama ${listaPessoas[i].nome} ${listaPessoas[i].sobrenome} e tem ${listaPessoas[i].idade} anos e não é maior de idade.`)
+        }
+
+    }
+}
+
+function verficarNome(listaPessoas) {
+    for (let i = 0; i < listaPessoas.length; i++) {
+        let nomeCompleto = listaPessoas[i].nome + " " + listaPessoas[i].sobrenome
+        let quantidade = nomeCompleto.length
+        if (quantidade < 3) {
+            console.log("Erro! Nome invalido, nome e sobrenome devem ser maiores que 3 caracteres." + nomeCompleto)
+        }
+        if (quantidade > 50) {
+            console.log("Erro! Nome invalido, nome e sobrenome devem ser menores que 50 caracteres." + nomeCompleto)
+        }
+    }
+}
+
+function quantidadeDePessoas(listaPessoas) {
+    console.log(`Existe ${listaPessoas.length} pessoas nesta lista`)
+}
+
+
 let listaPessoas = [
     {
         nome: "Fulano",
@@ -26,3 +55,7 @@ let listaPessoas = [
         idade: 13
     }
 ]
+maiorIdade(listaPessoas)
+quantidadeDePessoas(listaPessoas)
+verficarNome(listaPessoas)
+
