5cee31cc006ab143caa31dd6642f18d82a8a9e47
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-05-25 22:51:37 -0300

    Primeira tentativa

---
+++ Listas/Lista_8/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express
+const port = 7000
+
+app.request(express.json())
+
+let bancoDeDadosProdutos = []
+
+app.get('/listar-produtos', (req, res) => {
+    return res.status(200).json(bancoDeDadosProdutos)
+})
+
+app.post('/adicionar-produto', (req, res) => {
+    let body = req.body
+    console.log(body)
+    bancoDeDadosProdutos.push(body)
+    return res.status(201).json("Produto adicionado com sucesso!")
+})
+
+app.listen(port, function () {
+    console.log(`Servidor rodando http://localhost:${port}`)
+})
