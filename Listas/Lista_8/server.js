b21736669b40f5b68c47417a88c69eff7b995304
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-05-26 22:01:57 -0300

    Exercicio Concluido

--- Listas/Lista_8/server.js
+++ Listas/Lista_8/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = express
+const app = express()
 const port = 7000
 
-app.request(express.json())
+app.use(express.json())
 
 let bancoDeDadosProdutos = []
 
